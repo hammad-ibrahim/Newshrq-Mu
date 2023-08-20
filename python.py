66f703555e81c35c9ea0f998119a01a6c052e21c
hammad-ibrahim <hammad.ibrahim21994@gmail.com>
2023-08-20 18:55:41 +0300

    Adding Python script

---
+++ python.py
@@ -0,0 +1,8 @@
+# This is my First project in python
+
+# Print out New Hello
+NewShq = len(input("Pls Enter your Nmae Here:- \n "))
+FLett = NewShq[0]
+SLett = NewShq[3]
+ShrtName = str(FLett + SLett)
+print("This your name shorted to 2 Letter as you can see \n " + ShrtName)
